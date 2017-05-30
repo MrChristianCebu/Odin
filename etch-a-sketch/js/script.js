8e11e87f797bbbbeca8e9bfdd29a7467350f6d52
Mr. Christian <MrChristianCebu@users.noreply.github.com>
2017-05-30 16:26:15 -0700

    button now clears sketchpad

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
-	for (var i = 1; i <= 16; i++) {
-		for (var j = 1; j <= 16; j++) {
+	for (var i = 1; i <= 24; i++) {
+		for (var j = 1; j <= 24; j++) {
 			$('#container').append('<div class="pixel"></div>')
 		};
 	}
 });
 
 $(document).ready(function() {
-  $('div').hover(function() {
+  $('#button').click(function() {
+    $('div.pixel').css('background-color', '#aaafff')
+  });
+});
+
+$(document).ready(function() {
+  $('div.pixel').hover(function() {
     $(this).css('background-color', 'black');
   });
 });
